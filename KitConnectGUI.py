aa2c71131d7c0dff72330e1920b9d1ec849650f5
Cody Travis <chill@chillaspect.com>
2023-04-29 00:08:24 -0500

    Initial Commit
    Testing Midi Connections

---
+++ KitConnectGUI.py
@@ -0,0 +1,217 @@
+import json
+import os
+import datetime
+import sys
+from enum import Enum
+
+# PySide6 Imports
+from PySide6.QtWidgets import QApplication, QMainWindow, QStyle, QMessageBox, QTableWidgetItem, QPushButton, QHBoxLayout, QWidget, QToolButton
+from PySide6.QtCore import Qt, QSettings, QFile, QTextStream, QStandardPaths, QPoint, QTimer, QUrl, QSize, Signal, QObject, QRunnable, QThreadPool
+from PySide6.QtGui import QPixmap, QIcon, QDesktopServices, QIntValidator, QMovie
+
+import Resources_rc
+from UI_Components import Ui_MainWindow
+from td50x import LogLevel, TD50X
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        #Load UI Components
+        self.setupUi(self)
+        
+        #App Constants
+        self.geometryToRestore = None
+        self.repoUrl = QUrl("https://github.com/chillfactor032/kitconnect")
+        self.midi_devices = []
+        self.showDebug = True
+        self.showError = True
+        self.current_kit_id = 100
+        #Read Version File From Resources
+        version_file = QFile(":version.json")
+        version_file.open(QFile.ReadOnly)
+        text_stream = QTextStream(version_file)
+        version_file_text = text_stream.readAll()
+        self.version_dict = json.loads(version_file_text)
+        self.app_name = self.version_dict["product_name"]
+        self.version = self.version_dict["version"]
+        self.description = self.version_dict["description"]
+        self.author = self.version_dict["author"]
+        self.author_email = self.version_dict["author_email"]
+        self.project_name = self.app_name.title().replace(" ", "")
+        self.setWindowTitle(f"{self.app_name} {self.version}")
+        
+        #Load Settings
+        self.config_dir = QStandardPaths.writableLocation(QStandardPaths.ConfigLocation)
+        if(not os.path.isdir(self.config_dir)):
+            os.mkdir(self.config_dir)
+        self.ini_path = os.path.join(self.config_dir, f"kitconnectgui.ini").replace("\\", "/")
+        self.settings = QSettings(self.ini_path, QSettings.IniFormat)
+
+        # Button/Menu Signals Go Here
+        self.closeButton.clicked.connect(self.closeButtonClicked)
+        self.minimizeButton.clicked.connect(self.minimizedButtonClicked)
+        self.maximizeButton.clicked.connect(self.maximizedButtonClicked)
+        self.appLogMenuButton.clicked.connect(self.showLogPane)
+        self.homeMenuButton.clicked.connect(self.showHomePane)
+        self.midiLogMenuButton.clicked.connect(self.showMidiLogPane)
+        self.aboutButton.clicked.connect(self.showAbout)
+        self.githubButton.clicked.connect(self.showGithub)
+        self.refreshDevicesButton.clicked.connect(self.refreshDevices)
+        self.deviceComboBox.currentIndexChanged.connect(self.deviceSelected)
+        self.kitRightButton.clicked.connect(self.selectNextKitRight)
+        self.kitLeftButton.clicked.connect(self.selectNextKitLeft)
+        self.getCurKitButton.clicked.connect(self.refreshKit)
+
+        #TD-50X Object
+        self.td50x = TD50X()
+        self.td50x.signals.log.connect(self.log)
+        self.td50x.signals.midi_msg.connect(self.log_midi)
+
+        ## ThreadPool
+        self.threadpool = QThreadPool()
+
+        #Finally, Show the UI
+        self.stackedWidget.setCurrentWidget(self.kitsWidget)
+
+        geometry = self.settings.value(f"{self.project_name}/geometry")
+        window_state = self.settings.value(f"{self.project_name}/windowState")
+        if(geometry and window_state):
+            self.restoreGeometry(geometry) 
+            self.restoreState(window_state)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.show()
+        self.log("KitConnect started")
+
+    def refreshDevices(self):
+        print("Refresh Devices")
+        self.deviceComboBox.clear()
+        self.deviceComboBox.addItem("Select a device", None)
+        self.midi_devices = self.td50x.get_devices()
+        for device in self.midi_devices:
+            self.deviceComboBox.addItem(device["desc"], device)
+
+    def deviceSelected(self, index):
+        if index <= 0:
+            return
+        print(f"Device Selected: {index}")
+        device = self.deviceComboBox.currentData()
+        print(device)
+
+    def refreshKit(self):
+        print("Refresh Kit")
+
+    def selectNextKitLeft(self):
+        print("Select Next Kit <")
+        self.current_kit_id = (self.current_kit_id - 1) % 128
+        msg = [0xC9, self.current_kit_id, 0x00, 0x7F]
+        self.td50x.send_msg(msg)
+
+    def selectNextKitRight(self):
+        print("Select Next Kit >")
+        self.current_kit_id = (self.current_kit_id - 1) % 128
+        msg = [0xC9, self.current_kit_id, 0x00, 0x7F]
+        self.td50x.send_msg(msg)
+        
+    def showAbout(self):
+        repo = self.repoUrl.toString()
+        text = f"KitConnect TD-50X Midi GUI Client\n\n{self.description}\n\nGitHub: {repo}\nAuthor: {self.author} <{self.author_email}>"
+        mb = QMessageBox(QMessageBox.Icon.Information, "About", text)
+        mb.setTextInteractionFlags(Qt.TextSelectableByMouse)
+        mb.exec()
+
+    def showGithub(self):
+        QDesktopServices.openUrl(self.repoUrl)
+
+    def showHomePane(self):
+        self.stackedWidget.setCurrentWidget(self.kitsWidget)
+
+    def showLogPane(self):
+        self.stackedWidget.setCurrentWidget(self.logWidget)
+
+    def showMidiLogPane(self):
+        self.stackedWidget.setCurrentWidget(self.midiWidget)
+
+    def log_midi(self, type_int, incoming, data):
+        msg = " > "
+        if incoming:
+            msg = " < "
+        msg += str(TD50X.to_str(data))
+        msg = f'<span style="">{msg}</span>'
+        self.midiLogBrowser.append(msg)
+
+    def log(self, msg, level=LogLevel.INFO):
+        print(msg)
+        if not msg:
+            return
+        if level == LogLevel.ERROR:
+            if not self.showErrors:
+                return
+            style = "color: #cc0000;"
+        elif level == LogLevel.DEBUG:
+            if not self.showDebug:
+                return
+            style = "color: #006600;"
+        else:
+            style = "color: #000000;"
+        now = datetime.datetime.now()
+        timestamp = now.strftime("%H:%M:%S")
+        msg = f'<span style="{style}">{timestamp} - {msg}</span>'
+        self.logBrowser.append(msg)
+
+    def mousePressEvent(self, event):
+        globalPos = event.globalPosition().toPoint()
+        relativePos = event.position().toPoint()
+        source = self.childAt(relativePos)
+        if source == self.titleLabel:
+            self.oldPos = globalPos
+
+    def mouseMoveEvent(self, event):
+        globalPos = event.globalPosition().toPoint()
+        relativePos = event.position().toPoint()
+        source = self.childAt(relativePos)
+        if source == self.titleLabel:
+            delta = QPoint(globalPos - self.oldPos)
+            self.move(self.x() + delta.x(), self.y() + delta.y())
+            self.oldPos = globalPos
+
+    def minimizedButtonClicked(self):
+        self.setWindowState(Qt.WindowMinimized)
+
+    def maximizedButtonClicked(self):
+        if self.isMaximized():
+            #Restore the non-maximized window geometry
+            self.restoreGeometry(self.geometryToRestore)
+        else:
+            #Save window geometry before minimizing
+            self.geometryToRestore = self.saveGeometry()
+            self.setWindowState(Qt.WindowMaximized)
+
+    def closeButtonClicked(self):
+        self.close()
+
+    # App is closing, cleanup
+    def closeEvent(self, evt=None):
+        # Remember the size and position of the GUI
+        self.settings.setValue(f"{self.project_name}/geometry", self.saveGeometry())
+        self.settings.setValue(f"{self.project_name}/windowState", self.saveState())
+        self.settings.sync()
+        evt.accept()
+
+
+# Start the PySide6 App
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    version_file = QFile(":version.json")
+    version_file.open(QFile.ReadOnly)
+    text_stream = QTextStream(version_file)
+    version_file_text = text_stream.readAll()
+    version_dict = json.loads(version_file_text)
+    org_name = version_dict["company_name"]
+    app_name = version_dict["product_name"]
+    version = version_dict["version"]
+    app.setOrganizationName(org_name)
+    app.setApplicationName(app_name)
+    app.setApplicationVersion(version)
+    window = MainWindow()
+    sys.exit(app.exec())
---
+++ td50x.py
@@ -0,0 +1,207 @@
+#Suppress the hello message from PyGame
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+
+#Python Imports
+import sys
+import os
+import time
+import pygame.midi
+from enum import Enum
+
+#PySide6 Imports
+from PySide6.QtCore import QRunnable, Signal, Slot, QObject
+
+#Log Levels
+class LogLevel(Enum):
+    INFO = 0
+    ERROR = 10
+    DEBUG = 20
+    
+class TD50X(QRunnable):
+
+    MODEL_TD50X =  [0, 0, 0, 0, 7]
+    COMMAND_RQ1 = 0x11
+    STATUS_SYSEX = 0xF0
+    STATUS_EOX = 0xF7
+    STATUS_TIMING_CLOCK = 0xF8
+    STATUS_PROGRAM_CHANGE = 0xC9
+    VENDOR_ID_ROLAND = 0x41
+    DEVICE_ID = 0x10
+    TARGET_DEVICE_NAME = "TD-50X"
+
+    class Signals(QObject):
+        done = Signal(bool)
+        log = Signal(str, LogLevel)
+        #Midi Msg TYPE, Incoming, Msg Data
+        midi_msg = Signal(int, bool, list)
+
+    def __init__(self, td50x_id=None):
+        if td50x_id is None:
+            self.td50x_id = TD50X.DEVICE_ID
+        else:
+            self.td50x_id = td50x_id
+        self.input_device_id = -1
+        self.output_device_id = -1
+        self.output_device = None
+        self.input_device = None
+        self.stopped = False
+        self.outbox = []
+        self.update = True
+        self.signals = self.Signals()
+        pygame.midi.init()
+    
+    @staticmethod
+    def to_str(msg):
+        new_msg = []
+        for x in range(0, len(msg)):
+            new_msg.append(hex(msg[x]))
+        return new_msg
+    
+    def set_input_device(self, device_id):
+        self.input_device_id = device_id
+
+    def set_output_device(self, device_id):
+        self.output_device_id = device_id
+
+    def set_device_id(self, device_id):
+        self.td50x_id = device_id
+
+    def get_devices(self):
+        devices = []
+        num_midi_devices = pygame.midi.get_count()
+        self.signals.log.emit(f"Found {num_midi_devices} MIDI devices", LogLevel.INFO)
+        input_device_id = None
+        output_device_id = None
+        for x in range(num_midi_devices):
+            device_info = pygame.midi.get_device_info(x)
+            device = {
+                "name": "",
+                "input_id": -1,
+                "output_id": -1,
+                "desc": ""
+            }
+            if not device_info:
+                continue
+            iface, dname, is_input, is_output, opened = device_info
+            device["name"] = dname.decode(encoding="UTF-8")
+            if is_input:
+                device["input_id"] = x
+            if is_output:
+                device["output_id"] = x
+            self.signals.log.emit(f"  [{x}] [{device['name']}] {is_input} {is_output}", LogLevel.DEBUG)
+            # Look for other devices with the same name
+            for y in range(x+1,num_midi_devices):
+                device_info = pygame.midi.get_device_info(y)
+                if not device_info:
+                    continue
+                iface, dname, is_input, is_output, opened = device_info
+                if device["name"] == dname.decode(encoding="UTF-8"):
+                    #Found a matching Device Name
+                    if is_input:
+                        device["input_id"] = y
+                    if is_output:
+                        device["output_id"] = y
+            devices.append(device)
+
+        #Create the "Descriptions"
+        for device in devices:
+            device["desc"] = device["name"]
+            # 4 Possibilities
+            #   Output Only
+            if device["input_id"] < 0 and device["output_id"] >= 0:
+                device["desc"] += " (output)"
+            #   Input Only
+            if device["input_id"] >= 0 and device["output_id"] < 0:
+                device["desc"] += " (input)"
+            #   Input + Output
+            if device["input_id"] >= 0 and device["output_id"] >= 0:
+                device["desc"] += " (input + output)"
+            #   No Input or Output?
+            if device["input_id"] < 0 and device["output_id"] < 0:
+                device["desc"] += " (No Input or Output - Invalid Device)"
+        print(devices)
+        return devices
+            
+
+    def is_connected(self):
+        pass
+    
+    def update_devices(self, input_device_id, output_device_id):
+        self.input_device_id = input_device_id
+        self.output_device_id = output_device_id
+        self.update = True
+
+    def send_msg(self, msg):
+        self.outbox.append(msg)
+
+    def stop_midi(self):
+        self.stopped = True
+
+    #Handle an incoming message
+    def handle_msg(self, msg, timestamp):
+        self.signals.midi_msg.emit(0, True, msg)
+        print(f'{[f"{d:02x}" for d in msg]} {timestamp * 1e-3 : .3f}')
+
+    @Slot()
+    def run(self):
+        if self.input_device_id < 0 or self.output_device_id < 0:
+            self.signals.log("Midi device not set.", LogLevel.INFO)
+            return
+        self.input_device = pygame.midi.Input(self.input_device_id)
+        self.output_device = pygame.midi.Output(self.output_device_id)
+        sysex_response_buffer = None
+        self.signals.log.emit("TD-50X Midi Client Started", LogLevel.INFO)
+        while self.stopped == False:
+            #If no devices specified just do nothing
+            if self.input_device < 0 or self.output_device < 0 or self.input_device is None or self.output_device is None:
+                time.sleep(0.01)
+                continue
+
+            #If devices have changed, update
+            if self.update:
+                self.signals.log.emit("Updating Midi Devices", LogLevel.INFO)
+                self.output_device.abort()
+                self.output_device.stop()
+                self.input_device.close()
+                self.input_device = pygame.midi.Input(self.input_device_id)
+                self.output_device = pygame.midi.Output(self.output_device_id)
+                self.signals.log.emit(f"Updated midi devices to in=[{self.input_device}] out=[{self.output_device_id}]", LogLevel.DEBUG)
+            #Write a message from the outbox if its there
+            if len(self.outbox) > 0:
+                self.signals.midi_msg.emit(0, False, self.outbox[0])
+                if self.outbox[0][0] == TD50X.STATUS_SYSEX:
+                    self.signals.log.emit("Sending SysEx Msg: " + TD50X.to_str(self.outbox[0]), LogLevel.DEBUG)
+                    self.output_device.write_sys_ex(0, self.outbox[0])
+                else:
+                    self.signals.log.emit("Sending Midi Msg: " + TD50X.to_str(self.outbox[0]), LogLevel.DEBUG)
+                    self.output_device.write([[self.outbox[0], 0]])
+
+            #Read Some Events
+            event_list = pygame.midi.Input.read(self.input_device, 16)
+            for event in event_list:
+                data, timestamp = event
+                if sysex_response_buffer is not None:
+                    sysex_response_buffer.extend(data)
+                    if TD50X.STATUS_EOX in data:
+                        self.handle_msg(sysex_response_buffer, timestamp)
+                        sysex_response_buffer = None
+                elif data[0] == TD50X.STATUS_SYSEX:
+                    #Begin recv a sysex msg
+                    sysex_response_buffer = data
+                elif data[0] == TD50X.STATUS_TIMING_CLOCK:
+                    continue  # clock sync message
+                else:
+                    #Handle one-packet msg
+                    self.handle_msg(data, timestamp)
+
+        #Clean up if stopped
+        self.output_device.abort()
+        self.output_device.stop()
+        self.input_device.close()
+        self.signals.log.emit("TD-50X Midi Client Stopped", LogLevel.INFO)
+        
+
+
+
+    
