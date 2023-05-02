16511e7438bc06d8dbb919224f02cae2209106cd
Cody Travis <chill@chillaspect.com>
2023-05-01 22:32:44 -0500

    Added TestClient

---
+++ MidiConnection.py
@@ -0,0 +1,159 @@
+#Suppress the hello message from PyGame
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+
+#Python Imports
+from threading import Thread
+import pygame.midi
+import time
+import random
+
+class MidiConnection():
+
+    COMMAND_RQ1 = 0x11
+    STATUS_SYSEX = 0xF0
+    STATUS_EOX = 0xF7
+    STATUS_TIMING_CLOCK = 0xF8
+    STATUS_PROGRAM_CHANGE = 0xC9
+
+    def __init__(self, input_device_id, output_device_id, recv_msg_callback=None):
+        super(MidiConnection, self).__init__()
+        self.input_device_id = input_device_id
+        self.output_device_id = output_device_id
+        self.recv_msg_callback = recv_msg_callback
+        self.output_device = None
+        self.input_device = None
+        self.stopped = False
+        self.outbox = []
+    
+    def handle_msg(self, data, timestamp):
+        if self.recv_msg_callback is not None:
+            self.recv_msg_callback(data,timestamp)
+        else:
+            print(f'{[f"{d:02x}" for d in data]} {timestamp * 1e-3 : .3f}')
+    
+    def test_recv_msgs(self):
+        start = time.time()
+        elapsed = 0
+        rand = 0
+        while not self.stopped:
+            rand = random.random()
+            #Send a msg from the outbox
+            if len(self.outbox) > 0:
+                msg = self.outbox.pop(0)
+                print("> "+str(msg))
+            elapsed = time.time()-start
+            if rand > 0.90:
+                self.handle_msg([0x03, 0x04, 0x05, 0x06], round(elapsed*1000))
+            time.sleep(rand)
+        print("done")
+
+    def stop(self):
+        self.stopped = True
+        if self.thread is not None:
+            self.thread.join()
+
+    def start_test(self):
+        self.thread = Thread(target=self.test_recv_msgs, daemon=True).start()
+
+    def start(self):
+        self.thread = Thread(target=self.run, daemon=True).start()
+
+    def run(self):
+        if pygame.midi.get_init() == False:
+            pygame.midi.init()
+        input_device = pygame.midi.Input(self.input_device_id)
+        output_device = pygame.midi.Output(self.output_device_id)
+
+        while not self.stopped:
+            #Send a msg from the outbox
+            if len(self.outbox) > 0:
+                if self.outbox[0][0] == MidiConnection.STATUS_SYSEX:
+                    #Send a SysEx Msg
+                    output_device.write_sys_ex(0,self.outbox[0])
+                else:
+                    #Send a Msg
+                    output_device.write([[self.outbox[0], 0]])
+
+            #Read Some Events
+            event_list = pygame.midi.Input.read(input_device, 16)
+            for event in event_list:
+                data, timestamp = event
+                if sysex_response_buffer is not None:
+                    sysex_response_buffer.extend(data)
+                    if MidiConnection.STATUS_EOX in data:
+                        self.handle_msg(sysex_response_buffer, timestamp)
+                        sysex_response_buffer = None
+                elif data[0] == MidiConnection.STATUS_SYSEX:
+                    #Begin recv a sysex msg
+                    sysex_response_buffer = data
+                elif data[0] == MidiConnection.STATUS_TIMING_CLOCK:
+                    continue  # clock sync message
+                else:
+                    #Handle one-packet msg
+                    self.handle_msg(data, timestamp)
+
+    #Add a msg to the outbox
+    def send_msg(self, msg):
+        self.outbox.append(msg)
+
+    @staticmethod
+    def get_devices():
+        devices = []
+        if pygame.midi.get_init() == False:
+            pygame.midi.init()
+        num_midi_devices = pygame.midi.get_count()
+        input_device_id = None
+        output_device_id = None
+        for x in range(num_midi_devices):
+            device_info = pygame.midi.get_device_info(x)
+            device = {
+                "name": "",
+                "input_id": -1,
+                "output_id": -1,
+                "desc": ""
+            }
+            if not device_info:
+                continue
+            iface, dname, is_input, is_output, opened = device_info
+            device["name"] = dname.decode(encoding="UTF-8")
+            if is_input:
+                device["input_id"] = x
+            if is_output:
+                device["output_id"] = x
+            # Look for other devices with the same name
+            for y in range(x+1,num_midi_devices):
+                device_info = pygame.midi.get_device_info(y)
+                if not device_info:
+                    continue
+                iface, dname, is_input, is_output, opened = device_info
+                if device["name"] == dname.decode(encoding="UTF-8"):
+                    #Found a matching Device Name
+                    if is_input:
+                        device["input_id"] = y
+                    if is_output:
+                        device["output_id"] = y
+            devices.append(device)
+
+        #Create the "Descriptions"
+        for device in devices:
+            device["desc"] = device["name"]
+            # 4 Possibilities
+            #   Output Only
+            if device["input_id"] < 0 and device["output_id"] >= 0:
+                device["desc"] += " (output)"
+            #   Input Only
+            if device["input_id"] >= 0 and device["output_id"] < 0:
+                device["desc"] += " (input)"
+            #   Input + Output
+            if device["input_id"] >= 0 and device["output_id"] >= 0:
+                device["desc"] += " (input + output)"
+            #   No Input or Output?
+            if device["input_id"] < 0 and device["output_id"] < 0:
+                device["desc"] += " (No Input or Output - Invalid Device)"
+        return devices
+        
+
+
+
+    
---
+++ MidiTestClient.py
@@ -0,0 +1,77 @@
+#Suppress the hello message from PyGame
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+
+from MidiConnection import MidiConnection
+import sys
+
+#Vars
+device = None
+selection = -1
+incoming_msg_buffer = []
+
+#Prepared Midi Messages
+msgs = [
+    {
+        "desc": "Program Change Kit 100",
+        "msg": [0xC9, 0x99, 0x00, 0x7F]
+    },
+    {
+        "desc": "Program Change Kit 30",
+        "msg": [0xC9, 29, 0x00, 0x7F]
+    }
+]
+
+#Functions
+def quit(code):
+    sys.exit(code)
+
+def recv(msg, timestamp):
+    print("< "+str(msg) + f" {timestamp}")
+
+#Get Midi Devices
+devices = MidiConnection.get_devices()
+
+#Select Midi Device
+while device is None:
+    #Get Devices
+    for x in range(len(devices)):
+        print(f"[{x}] {devices[x]['desc']}")
+
+    #print options
+    try:
+        selection = int(input("Select a Midi Device:"))
+        print()
+    except ValueError as ve:
+        print("Invalid Answer. Make it an integer please.")
+        continue
+    except KeyboardInterrupt as ke:
+        print("\nKeyboard Interrupt. Goodbye.")
+        quit(0)
+    print(f"You selected [{selection}] {devices[selection]['name']}\n")
+    device = devices[selection]
+
+#Create MidiConnection
+midi = MidiConnection(device["input_id"],device["output_id"],recv)
+midi.start()
+
+#Send / Recv Midi Packets
+while True:
+    #print options
+    i = 0
+    print("----------------------")
+    for i in range(len(msgs)):
+        option = msgs[i]
+        print(f"[{i}] {option['desc']}")
+    print("----------------------")
+    try:
+        selection = int(input("Make Selection:\n"))
+    except ValueError as ve:
+        continue
+    except KeyboardInterrupt as ke:
+        print("\nKeyboard Interrupt. Goodbye.")
+        quit(0)
+    if selection >= 0 and selection < len(msgs):
+        midi.send_msg(msgs[selection]["msg"])
+    else:
+        print("Bad selection try again.")
